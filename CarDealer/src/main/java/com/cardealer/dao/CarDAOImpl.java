a02e32cf884b018a7166043c61d10a4dea8dd5ce
Magda <estelle2501@gmail.com>
2017-03-17 19:07:23 +0100

    Added addCar in CarDAOImpl (Insert SQL) Added testAddCar in testCarDAOImpl

--- CarDealer/src/main/java/com/cardealer/dao/CarDAOImpl.java
+++ CarDealer/src/main/java/com/cardealer/dao/CarDAOImpl.java
@@ -2,22 +2,52 @@ package com.cardealer.dao;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-public class CarDAOImpl {
+import com.cardealer.model.Car;
+
+public class CarDAOImpl implements CarDAO {
 
 	private final String url = "jdbc:postgresql://localhost:5432/cardealer";
 	private final String user = "postgres";
 	private final String password = "marchewka3";
 
-	public Connection connect() {
-		Connection conn = null;
-		try {
-			conn = DriverManager.getConnection(url, user, password);
-			System.out.println("Connected to PostgresSQL successfully");
-		} catch (SQLException e) {
-			System.out.println(e.getMessage());
+	public Connection connect() throws SQLException {
+		return DriverManager.getConnection(url, user, password);
+	}
+
+	public void addCar(Car car) {
+		String SQL = "INSERT INTO cars (model, manufacture_year)"
+				+ "VALUES (?,?)";
+		
+		try (Connection conn = connect();
+				PreparedStatement pstmt = conn.prepareStatement(SQL, 
+						Statement.RETURN_GENERATED_KEYS)){
+			pstmt.setString(1, car.getModel());
+			pstmt.setInt(2, car.getManufactureYear());
+			
+			pstmt.executeUpdate();
+
+		}catch(SQLException ex){
+			System.out.println(ex.getMessage());
 		}
-		return conn;
+	
+	}
+
+	public void updateCar(Car c) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public Car getCarById(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public void deleteCar(int id) {
+		// TODO Auto-generated method stub
+
 	}
 }
--- CarDealer/src/test/java/com/cardealer/dao/testCarDAOImpl.java
+++ CarDealer/src/test/java/com/cardealer/dao/testCarDAOImpl.java
@@ -2,7 +2,12 @@ package com.cardealer.dao;
 
 import static org.junit.Assert.*;
 
+import java.sql.SQLException;
+
+import org.junit.Ignore;
 import org.junit.Test;
+
+import com.cardealer.model.Car;
 
 public class testCarDAOImpl {
 
@@ -10,7 +15,35 @@ public class testCarDAOImpl {
 	@Test
 	public void testConnect() {
 		CarDAOImpl carDAOImpl = new CarDAOImpl();
-		carDAOImpl.connect();
+		Exception ex = null;
+		try {
+			carDAOImpl.connect();
+		} catch (SQLException e) {
+			ex = e;
+		}
+		assertEquals(null, ex);
+	}
+
+	@Test
+	public void testAddCar() {
+		Car car = new Car();
+		car.setManufactureYear(2009);
+		car.setModel("Alfa Romeo 159");
+
+		CarDAOImpl carDAOImpl = new CarDAOImpl();
+
+		carDAOImpl.addCar(car);
+
+	}
+
+	@Ignore
+	@Test
+	public void testAddCarWithWrongAttributes() {
+		Car car = new Car();
+		car.setManufactureYear(20099);
+		car.setModel("Alfa Romeo 159");
+		CarDAOImpl carDAOImpl = new CarDAOImpl();
+		carDAOImpl.addCar(car);
 	}
 
 }
