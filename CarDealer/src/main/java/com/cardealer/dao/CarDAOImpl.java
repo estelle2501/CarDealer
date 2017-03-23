ee3b7f995f491e8265ac523fb27fd92a4facc232
Magda <estelle2501@gmail.com>
2017-03-23 20:08:25 +0100

    updateCar and deleteCar added in CarDAOImpl

--- CarDealer/src/main/java/com/cardealer/dao/CarDAOImpl.java
+++ CarDealer/src/main/java/com/cardealer/dao/CarDAOImpl.java
@@ -1,11 +1,29 @@
 package com.cardealer.dao;
 
+import java.io.InputStream;
+import java.io.Reader;
+import java.math.BigDecimal;
+import java.net.URL;
+import java.sql.Array;
+import java.sql.Blob;
+import java.sql.Clob;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.DriverManager;
+import java.sql.NClob;
+import java.sql.ParameterMetaData;
 import java.sql.PreparedStatement;
+import java.sql.Ref;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.RowId;
 import java.sql.SQLException;
+import java.sql.SQLWarning;
+import java.sql.SQLXML;
 import java.sql.Statement;
+import java.sql.Time;
+import java.sql.Timestamp;
+import java.util.Calendar;
 import java.util.concurrent.locks.StampedLock;
 
 import com.cardealer.model.Car;
@@ -21,13 +39,13 @@ public class CarDAOImpl implements CarDAO {
 	}
 
 	public int addCar(Car car) {
-		String SQL = "INSERT INTO cars (model, manufacture_year)"
+		String SQLinsert = "INSERT INTO cars (model, manufacture_year)"
 				+ "VALUES (?,?)";
 
 		int id = 0;
 
 		try (Connection conn = connect();
-				PreparedStatement statement = conn.prepareStatement(SQL,
+				PreparedStatement statement = conn.prepareStatement(SQLinsert,
 						Statement.RETURN_GENERATED_KEYS)) {
 			statement.setString(1, car.getModel());
 			statement.setInt(2, car.getManufactureYear());
@@ -53,25 +71,36 @@ public class CarDAOImpl implements CarDAO {
 
 	}
 
-	public void updateCar(Car c) {
-		// TODO Auto-generated method stub
+	public void updateCar(Car car) {
+		String SQLupdate = "UPDATE cars SET model = ?, manufacture_year = ? WHERE id = ?";
+
+		try (Connection conn = connect();
+				PreparedStatement statement = conn.prepareStatement(SQLupdate)) {
+			statement.setString(1, car.getModel());
+			statement.setInt(2, car.getManufactureYear());
+			statement.setInt(3, car.getId());
+
+			statement.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 
 	public Car getCarById(int id) {
-		String SQL = "SELECT id, model, manufacture_year "
-				+ "FROM cars "
+		String SQLgetCar = "SELECT id, model, manufacture_year " + "FROM cars "
 				+ "WHERE id = ?";
 		Car car = new Car();
 
 		try (Connection conn = connect();
-				PreparedStatement statement = conn.prepareStatement(SQL)) {
+				PreparedStatement statement = conn.prepareStatement(SQLgetCar)) {
 			statement.setInt(1, id);
 			ResultSet rs = statement.executeQuery();
-			while(rs.next()){
+			while (rs.next()) {
 				car.setId(rs.getInt("id"));
 				car.setModel(rs.getString("model"));
-				car.setManufactureYear(rs.getInt("manufacture_year"));				
+				car.setManufactureYear(rs.getInt("manufacture_year"));
 			}
 
 		} catch (SQLException ex) {
@@ -82,7 +111,17 @@ public class CarDAOImpl implements CarDAO {
 	}
 
 	public void deleteCar(int id) {
-		// TODO Auto-generated method stub
+		String SQLDelete = "DELETE FROM cars WHERE id = ?";
+
+		try (Connection conn = connect();
+				PreparedStatement statement = conn.prepareStatement(SQLDelete)) {
+
+			statement.setInt(1, id);
+
+			statement.executeUpdate();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
 
 	}
 }
--- CarDealer/src/test/java/com/cardealer/dao/testCarDAOImpl.java
+++ CarDealer/src/test/java/com/cardealer/dao/testCarDAOImpl.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.*;
 
 import java.sql.SQLException;
 
+import javax.transaction.xa.XAResource;
+
 import org.junit.Ignore;
 import org.junit.Test;
 
@@ -11,10 +13,13 @@ import com.cardealer.model.Car;
 
 public class testCarDAOImpl {
 
+	
+	CarDAOImpl carDAOImpl = new CarDAOImpl();
+	
 	// Testing simple jdbc postgres connection
 	@Test
 	public void testConnect() {
-		CarDAOImpl carDAOImpl = new CarDAOImpl();
+		
 		Exception ex = null;
 		try {
 			carDAOImpl.connect();
@@ -29,8 +34,6 @@ public class testCarDAOImpl {
 		Car car = new Car();
 		car.setManufactureYear(2009);
 		car.setModel("Alfa Romeo 159");
-		
-		CarDAOImpl carDAOImpl = new CarDAOImpl();
 
 		int id = carDAOImpl.addCar(car);
 		assertNotEquals(0, id);
@@ -42,22 +45,65 @@ public class testCarDAOImpl {
 		Car car = new Car();
 		car.setManufactureYear(20099);
 		car.setModel("Alfa Romeo 159");
-		
-		CarDAOImpl carDAOImpl = new CarDAOImpl();
-		
+
 		int id = carDAOImpl.addCar(car);
 		assertEquals(0, id);
 	}
-	
+
 	@Test
-	public void testGetCarByID(){
+	public void testGetCarByID() {
 		int id = 12;
-		CarDAOImpl carDAOImpl = new CarDAOImpl();
 		Car car = null;
 		car = carDAOImpl.getCarById(id);
-		System.out.println(car);
 		assertEquals(id, car.getId());
+
+	}
+
+	@Test
+	public void testUpdateCar() {
+		Car car = new Car();
+		car.setManufactureYear(2018);
+		car.setModel("Alfa Romeo Julia");
+
+		/* I don't like the fact I have to setId to the car 
+		 * shouldn't addCar do this?
+		 * And it is still lacking error handling
+		 * what if there is a mistake when adding to DB?
+		 * there should be exeptions and notification
+		 * that car was not added
+		 */
+
+		car.setId(carDAOImpl.addCar(car));
+
+		System.out.println("Car: " + car);
+
+		car.setManufactureYear(2017);
+		carDAOImpl.updateCar(car);
+
+		System.out
+				.println("Updated car: " + carDAOImpl.getCarById(car.getId()));
+
+	}
+	
+	
+	@Test
+	public void testDeleteCar(){
+		Car car = new Car();
+		car.setManufactureYear(2013);
+		car.setModel("Alfa Romeo Mito");
+
+		car.setId(carDAOImpl.addCar(car));
 		
+		System.out.println(car);
+		
+		/*
+		 * What is better
+		 * deleteCar (id) or deleteCar (Car) ?
+		 */
+		
+		carDAOImpl.deleteCar(car.getId());
+		
+		System.out.println(carDAOImpl.getCarById(car.getId()));
 		
 	}
 
