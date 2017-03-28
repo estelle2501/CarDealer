263f10b20fd40ff6409fc43def9f1220db6f1b64
Magda <estelle2501@gmail.com>
2017-03-28 22:41:12 +0200

    Added readCarsFromCSV

--- CarDealer/src/main/java/com/cardealer/csv/adapter/CSVAdapter.java
+++ CarDealer/src/main/java/com/cardealer/csv/adapter/CSVAdapter.java
@@ -1,26 +1,56 @@
 package com.cardealer.csv.adapter;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import com.cardealer.model.Car;
 
 public class CSVAdapter {
 
-	private final String filePath = "C:/Users/Magda/git/CarDealerRepository/cars.csv";
-	private final String separator = ",";
+	private final String FILE_PATH = "C:/Users/Magda/git/CarDealerRepository/cars.csv";
+	private final String LINE_SEPARATOR = "\\s*,\\s*";
 	String line = "";
 
 	public void readCSV() {
-		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+		try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
 
 			while ((line = br.readLine()) != null) {
-				String[] cars = line.split(separator);
+				String[] cars = line.split(LINE_SEPARATOR);
 				System.out.println(cars[0]);
 			}
 
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+
+	}
+
+	public List<Car> readCarsFromCSV() {
+
+		List<Car> carsList = new ArrayList<>();
+
+		try (BufferedReader br = new BufferedReader(new FileReader(FILE_PATH))) {
+
+			while ((line = br.readLine()) != null) {
+
+				List<String> csvColumnsList = Arrays.asList(line.split("\\s*,\\s*"));
+				Car car = new Car();
+				car.setModel(csvColumnsList.get(0));
+				car.setManufactureYear(Integer.parseInt(csvColumnsList.get(1)));
+				carsList.add(car);
+			}
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return carsList;
 
 	}
 }
--- CarDealer/src/test/java/com/cardealer/csv/adapter/TestCSVAdapter.java
+++ CarDealer/src/test/java/com/cardealer/csv/adapter/TestCSVAdapter.java
@@ -2,7 +2,11 @@ package com.cardealer.csv.adapter;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
+
+import com.cardealer.model.Car;
 
 public class TestCSVAdapter {
 
@@ -10,6 +14,16 @@ public class TestCSVAdapter {
 	public void testReadCSV() {
 		CSVAdapter ca = new CSVAdapter();
 		ca.readCSV();
+	}
+	
+	@Test
+	public void testreadCarsFromCSV() {
+		CSVAdapter ca = new CSVAdapter();
+		List<Car> carsList = ca.readCarsFromCSV();
+		for (Car c : carsList){
+			System.out.println(c.toString());
+		}
+		
 	}
 
 }
