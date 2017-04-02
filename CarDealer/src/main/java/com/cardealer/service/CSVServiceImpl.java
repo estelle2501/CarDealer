878b6df911937b4a9cce7b23de164d431c5107b7
Magda <estelle2501@gmail.com>
2017-04-02 11:01:27 +0200

    addCars() in CarDAO/CarDAOImpl CSVImportCars() in CSVServiceImpl

--- CarDealer/src/main/java/com/cardealer/service/CSVServiceImpl.java
+++ CarDealer/src/main/java/com/cardealer/service/CSVServiceImpl.java
@@ -3,15 +3,17 @@ package com.cardealer.service;
 import java.util.List;
 
 import com.cardealer.csv.adapter.CSVAdapter;
+import com.cardealer.dao.CarDAOImpl;
 import com.cardealer.model.Car;
 
 public class CSVServiceImpl implements CSVService {
 
+	CarDAOImpl carDAOImpl = new CarDAOImpl();
 
 	public void CSVImportCars() {
 		
 		List<Car> carsList = CSVAdapter.readCarsFromCSV();
-		
+		carDAOImpl.addCars(carsList);		
 	}
 
 }
